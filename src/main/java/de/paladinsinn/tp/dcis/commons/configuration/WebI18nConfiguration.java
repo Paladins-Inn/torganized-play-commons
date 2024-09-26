9eb46aa8e416c81bee9b5845dbe615753f0c9323
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-09-27 01:39:25 +0200

    WIP.

---
+++ src/main/java/de/paladinsinn/tp/dcis/commons/configuration/WebI18nConfiguration.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.paladinsinn.tp.dcis.commons.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import de.kaiserpfalzedv.commons.spring.i18n.KaiserpfalzMessageSource;
+
+@Configuration
+public class WebI18nConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        return new KaiserpfalzMessageSource();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor result = new LocaleChangeInterceptor();
+        result.setParamName("lang");
+        return result;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    
+}
