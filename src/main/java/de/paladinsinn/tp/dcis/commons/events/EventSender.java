ed5993d59104a81bc18ab4e820e8258e49231b19
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-11-30 11:37:13 +0100

    Added an AMQP sender based on spring-boot rabbitmq integration.

---
+++ src/main/java/de/paladinsinn/tp/dcis/commons/events/EventSender.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2024. Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.paladinsinn.tp.dcis.commons.events;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * EventSender -- sending events via AMQP to RabbitMQ.
+ * <code>
+ * spring:
+ *   rabbitmq:
+ *     host: &lt;hostname&gt;
+ *     port: 5672
+ *     username: &lt;username&gt;
+ *     password: &lt:password&gt;
+ *     template:
+ *       retry:
+ *         enabled: true
+ *         initial-interval: 2s
+ * </code>
+ *
+ * @author klenkes74
+ * @since 30.11.24
+ */
+@Service
+@RequiredArgsConstructor
+@ToString
+@XSlf4j
+public class EventSender<T extends DcisBaseEvent> {
+  private final RabbitTemplate amqp;
+
+  public void send(Queue queue, T event) {
+    log.entry(queue, event);
+
+    amqp.convertAndSend(queue.getName(), event);
+
+    log.exit();
+  }
+}
---
+++ src/test/java/de/paladinsinn/kp/docs/commons/events/EventSenderTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2024. Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.paladinsinn.kp.docs.commons.events;
+
+
+import de.paladinsinn.tp.dcis.commons.events.EventSender;
+import lombok.ToString;
+import lombok.extern.slf4j.XSlf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * @author klenkes74
+ * @since 30.11.24
+ */
+@ExtendWith(SpringExtension.class)
+@ToString
+@XSlf4j
+public class EventSenderTest {
+
+  @Autowired
+  private EventSender<TestEvent> sut;
+
+  @Autowired
+  private RabbitTemplate rabbitTemplate;
+
+  private final EventSenderTestContextConfiguration config = new EventSenderTestContextConfiguration();
+
+  @TestConfiguration
+  static class EventSenderTestContextConfiguration {
+    @MockBean
+    static RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+      return rabbitTemplate;
+    }
+
+    @Bean
+    public EventSender<TestEvent> eventSender(RabbitTemplate rabbitTemplate) {
+      return new EventSender<>(rabbitTemplate);
+    }
+  }
+
+  @Test
+  public void shouldSendTheEventWithAMessageAndQueue() {
+    Queue target = Mockito.mock(Queue.class);
+    TestEvent event = new TestEvent(UUID.randomUUID());
+    log.entry(target, event);
+
+    sut.send(target, event);
+
+    verify(rabbitTemplate, times(1)).convertAndSend(target.getName(), event);
+
+    log.exit();
+  }
+}
